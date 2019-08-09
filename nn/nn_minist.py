1a9a8a80a17d30a75700085e3ac8e0e04e65c941
Jay-Jo <572501002@qq.com>
2019-08-09 19:40:01 +0800

    提交test1.html

--- nn/nn_minist.py
+++ nn/nn_minist.py
@@ -1,60 +1,80 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
+import matplotlib.pyplot as plt
 
 mnist=input_data.read_data_sets("../tmp/data",one_hot=True)
-pic_size=28
-input_node=pic_size**2
+
+input_node=784
 classes=10
-layer1=150
+layer1=100
 layer2=50
 batch=100
-lr=10e-3
-data_size=len(mnist.validation.labels)
-print(data_size)   #5000个数据
+lr=0.02
+
 
 ##############data
-x=tf.placeholder(tf.float32,shape=(None,input_node),name='x-in')
-y=tf.placeholder(tf.float32,shape=(None,classes),name='y')
 
+def full_connected():
+    with tf.variable_scope('data'):
+        x = tf.placeholder(tf.float32, [None, 784])
+        y = tf.placeholder(tf.float32, [None, 10])
 
-#############inference
+    ###定义前向传播过程
+    with tf.variable_scope('layer1'):
+        w1 = tf.Variable(tf.random_normal([input_node, layer1], stddev=1, seed=1), name='w1')  # 数据可能一个一个输入的
+        b1 = tf.Variable(tf.random_normal([layer1], stddev=1, seed=1), name='b1')
 
+    with tf.variable_scope('layer2'):
+        w2 = tf.Variable(tf.random_normal([layer1, 10], stddev=1, seed=1), name='w2')
+        b2 = tf.Variable(tf.random_normal([10], stddev=1, seed=1), name='b2')
 
-###定义前向传播过程
-with tf.variable_scope('layer1'):
-    w1 = tf.Variable(tf.random_normal([input_node, layer1], stddev=1, seed=1), name='w1')  # 数据可能一个一个输入的
-    b1 = tf.Variable(tf.random_normal([layer1], stddev=1, seed=1), name='b1')
-
-with tf.variable_scope('layer2'):
-    w2 = tf.Variable(tf.random_normal([layer1, layer2], stddev=1, seed=1), name='w2')
-    b2 = tf.Variable(tf.random_normal([layer2], stddev=1, seed=1), name='b2')
-with tf.variable_scope('layer3'):
-    w3 = tf.Variable(tf.random_normal([layer2, classes], stddev=1, seed=1), name='w3')  # 数据可能一个一个输入的
-    b3 = tf.Variable(tf.random_normal([classes], stddev=1, seed=1), name='b3')
-
-def two_network(nn_input):
     with tf.variable_scope('two_network'):
         s1 = tf.sigmoid((tf.matmul(x, w1)+b1))
-        s2= tf.sigmoid(tf.matmul(s1, w2)+ b2)
-        return tf.sigmoid(tf.matmul(s2, w3)+b3)
-net=two_network(x)
-y_pre=two_network(x)
+        y_pre=tf.matmul(s1, w2)+ b2
 
-# loss=-tf.reduce_mean(tf.add(tf.matmul(y,tf.log(y_pre)),tf.matmul((1-y),tf.log(1-y_pre))) )  ###
-cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_pre, 1))
-loss = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr, name='optimizer')
-train_op=optimizer.minimize(loss)
+    with tf.variable_scope('soft_cross'):
+        loss= tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_pre))
+    with tf.variable_scope('optimizer'):
+        train_op=tf.train.GradientDescentOptimizer(learning_rate=lr, name='optimizer').minimize(loss)
+# loss=tf.losses.softmax_cross_entropy(logits=y_pre,onehot_labels=y)####
+# loss = tf.reduce_mean(-tf.reduce_sum(y*tf.log(y_pre)))   #求交叉熵
+# cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_pre, 1))
+# loss = tf.reduce_mean(cross_entropy)
 
-################train
-sess=tf.Session()
-sess.run(tf.global_variables_initializer())
-for e in range(5000):
-# datafeed={x:mnist.validation.images,y:mnist.validation.labels}
-    start = (e * batch) % data_size
-    end = (start + batch+1) % data_size
-    sess.run(train_op,feed_dict={x:mnist.validation.images[start:end],y:mnist.validation.labels[start:end]})
-    # if e%500==0:
 
-sess.close()
+    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(y,1))   #在测试阶段，测试准确度计算
+    acc = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+    loss_arr=[]
+    acc_arr=[]
+    ################train
+    init = tf.initialize_all_variables()
+    with tf.Session() as sess:
+        sess.run(init)
+        for e in range(15000):
+        # datafeed={x:mnist.validation.images,y:mnist.validation.labels}
+            xs,ys=mnist.train.next_batch(batch)
+            # loss_arr.append(loss_numpy)
+            _,loss_val,accc=sess.run([train_op,loss,acc],feed_dict={x:xs,y:ys})
+            loss_arr.append(loss_val)
+            acc_arr.append(accc)
+            if e%500==0:
+                print(loss_val)
+
+                # y_pre_numpy=y_pre.eval(session=sess)
+                # print(y_pre_numpy)
+                # y_true_label=y.eval(session=sess)
+                # y_pre_labe=(y_pre_numpy)
+                # acc=np.mean(y_pre_labe==y_true_label)
+                print('step is ',(e,loss_val,accc))
+
+    sess.close()
+    plt.plot(loss_arr, label='loss')
+    plt.plot(acc_arr, label='acc')
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    full_connected()
+
